2c92a8d5e444ae6032b5f96f31a86020a4708ca4
C.A.Etsebeth <chris@mayimconsulting.com>
2026-08-22 09:05:27 +0200

    feat: add icons and resources_rc for initial deployment

--- mayim_tools/scripts/deploy.py
+++ mayim_tools/scripts/deploy.py
@@ -16,7 +16,7 @@ PLUGIN_NAME = "mayim_tools"
 
 # Update this path to match your QGIS plugins folder:
 QGIS_PLUGINS_DIR = Path(
-    r"C:\Users\{YOUR_USERNAME}\AppData\Roaming\QGIS\QGIS3"
+    r"C:\Users\caets\AppData\Roaming\QGIS\QGIS3"
     r"\profiles\default\python\plugins"
 )
 
